67b08a523354bc2479d92f3888c8eaadd4dccf9b
Jennings Zhang <dev@sl.jennin.xyz>
2024-09-30 19:02:40 -0400

    Add OpenAPI generated typescript-fetch client

---
+++ src/api/cubeGenerated/apis/index.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthTokenApi';
+export * from './ChrisAdminApi';
+export * from './ChrisinstanceApi';
+export * from './CommentsApi';
+export * from './ComputeresourcesApi';
+export * from './DefaultApi';
+export * from './DownloadtokensApi';
+export * from './FilebrowserApi';
+export * from './GrouppermissionsApi';
+export * from './GroupsApi';
+export * from './NoteApi';
+export * from './PacsApi';
+export * from './PipelinesApi';
+export * from './PlugininstancesApi';
+export * from './PluginsApi';
+export * from './PublicfeedsApi';
+export * from './SchemaApi';
+export * from './SearchApi';
+export * from './TaggingsApi';
+export * from './TagsApi';
+export * from './UserfilesApi';
+export * from './UserpermissionsApi';
+export * from './UsersApi';
